b11e62d7e2fb9d5ecfd95346f9228d123f989a44
sitoyithegeek <sitoyi.avuyile@gmail.com>
2026-03-13 22:48:39 +0200

    did the address and wastetocollect classes

--- src/main/java/za/ac/cput/domain/WasteCollector.java
+++ src/main/java/za/ac/cput/domain/WasteCollector.java
@@ -1,4 +1,79 @@
 package za.ac.cput.domain;
 
-public class WasteCollector {
+public class WasteCollector
+{
+ private String collectorId;
+ private String name;
+ private long contactNumber;
+ private  String assignedArea ;
+
+
+  private WasteCollector(Builder builder){
+     this.collectorId = builder.collectorId;
+     this.name = builder.name;
+     this.contactNumber = builder.contactNumber;
+     this.assignedArea  = builder.assignedArea;
+
+  }
+
+    public String getCollectorId() {
+        return collectorId;
+    }
+
+    public void setCollectorId(String collectorId) {
+        this.collectorId = collectorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getAssignedArea() {
+        return assignedArea;
+    }
+
+    public void setAssignedArea(String assignedArea) {
+        this.assignedArea = assignedArea;
+    }
+
+    public static class Builder{
+        private String collectorId;
+        private String name;
+        private long contactNumber;
+        private  String assignedArea ;
+
+        public Builder getCollectorId(String collectorId){
+          this.collectorId =  collectorId;
+          return this;
+        }
+        public Builder getName(String name){
+           this.name = name;
+           return this;
+        }
+        public  Builder  getContactNumber(long contactNumber){
+            this.contactNumber = contactNumber;
+            return this;
+        }
+        public Builder getAssignedArea(String assignedArea){
+            this.assignedArea = assignedArea;
+            return this;
+        }
+
+        public  WasteCollector build(){
+            return new WasteCollector(this);
+        }
+
+    }
 }
