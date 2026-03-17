040e96a5f1cb40d6bae5edefc7a927decbb2550e
IngaPlati <230126634@mycput.ac.za>
2026-03-17 11:49:33 +0200

    My current factory work before syncing with updated repo

--- src/main/java/za/ac/cput/factory/GeoLactionFactory.java
+++ src/main/java/za/ac/cput/factory/GeoLactionFactory.java
@@ -1,4 +1,15 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.GeoLocation;
+
 public class GeoLactionFactory {
+
+    public static GeoLocation createGeoLocation(double latitude, double longitude) {
+
+        return new GeoLocation.Builder()
+                .setLatitude(latitude)
+                .setLongitude(longitude)
+                .build();
+    }
 }
+
--- src/main/java/za/ac/cput/factory/PickUpScheduleFactory.java
+++ src/main/java/za/ac/cput/factory/PickUpScheduleFactory.java
@@ -1,4 +1,23 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.PickUpSchedule;
+
 public class PickUpScheduleFactory {
+
+    public static PickUpSchedule createPickupSchedule(
+            String scheduleId,
+            String collectionDay,
+            String collectionTime,
+            String area,
+            boolean reminderEnabled) {
+
+        return new PickUpSchedule.Builder()
+                .setScheduleId(scheduleId)
+                .setCollectionDay(collectionDay)
+                .setCollectionTime(collectionTime)
+                .setArea(area)
+                .setReminderEnabled(reminderEnabled)
+                .build();
+    }
 }
+
