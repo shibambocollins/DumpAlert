271bc11cd5d7cbcc124f15b2392a776c278f0c14
Phumelela142 <240040546@mycput.ac.za>
2026-03-26 10:10:47 +0200

    made changes to AvailabilitySlot.java

--- src/main/java/za/ac/cput/factory/AvailabilitySlotFactory.java
+++ src/main/java/za/ac/cput/factory/AvailabilitySlotFactory.java
@@ -1,4 +1,40 @@
 package za.ac.cput.factory;
+import za.ac.cput.entity.AvailabilitySlot;
+import za.ac.cput.entity.enums.SlotStatus;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
 
 public class AvailabilitySlotFactory {
+
+    public static AvailabilitySlot createSlot(LocalDate date,
+                                              LocalDateTime startTime,
+                                              LocalDateTime endTime) {
+
+        if (date == null || startTime == null || endTime == null) {
+            return null;
+        }
+
+
+        if (!endTime.isAfter(startTime)) {
+            return null;
+        }
+
+
+        if (!startTime.toLocalDate().equals(date)) {
+            return null;
+        }
+
+        String slotId = UUID.randomUUID().toString();
+
+        return new AvailabilitySlot.Builder()
+                .setSlotId(slotId)
+                .setDate(date)
+                .setStartTime(startTime)
+                .setEndTime(endTime)
+                .setStatus(SlotStatus.AVAILABLE)
+                .build();
+    }
 }
